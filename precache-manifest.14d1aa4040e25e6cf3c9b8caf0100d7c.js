b3e54789b0810a7de2e91d2914c583650c90512e
Den <fentau.line@gmail.com>
2020-05-17 12:02:06 +0300

    Updates

---
+++ precache-manifest.14d1aa4040e25e6cf3c9b8caf0100d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db643aff5ed1629fc7677a76684370eb",
+    "url": "/React_beta/index.html"
+  },
+  {
+    "revision": "455a05bf9a5873dbb156",
+    "url": "/React_beta/static/css/main.c4aee2d5.chunk.css"
+  },
+  {
+    "revision": "1710663f1fae81342094",
+    "url": "/React_beta/static/js/2.f9402cac.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/React_beta/static/js/2.f9402cac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455a05bf9a5873dbb156",
+    "url": "/React_beta/static/js/main.042b5c31.chunk.js"
+  },
+  {
+    "revision": "ad8d1c7831bc4be4ee0a",
+    "url": "/React_beta/static/js/runtime-main.acb8a36f.js"
+  }
+]);
